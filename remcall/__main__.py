192a076dc4d5ebbd26650f0ec7815947be9119f9
luphord <luphord@protonmail.com>
2017-11-09 17:43:19 +0000

    [cb15e74117] support selection of namespace in CLI

--- remcall/__main__.py
+++ remcall/__main__.py
@@ -18,13 +18,14 @@ def generate(args):
     with open(args.schema, mode='rb') as f:
         schema = read_schema(f)
     if args.language == 'csharp':
-        generator = CSharphCodeGenerator(schema)
+        generator = CSharphCodeGenerator(schema, args.namespace)
     else:
         raise NotImplementedError('Would love to be able to generate code for {}'.format(args.language))
     import sys
     generator.write_schema(sys.stdout)
 generate_parser = subparsers.add_parser('generate', description='Generate code from schema')
 generate_parser.add_argument('-l', '--language', help='Programming language to create code for', default='csharp', type=str, choices=['c', 'csharp', 'go', 'java'])
+generate_parser.add_argument('-n', '--namespace', help='Namespace to create code in', default='Remcall.Generated', type=str)
 generate_parser.add_argument('schema')
 generate_parser.set_defaults(func=generate)
 
