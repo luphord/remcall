d6b78ebe5bc88f88d43b36f5996791265796e021
luphord <luphord@protonmail.com>
2018-10-09 05:31:03 +0000

    first go test

---
+++ go/remcall/schema/enum_test.go
@@ -0,0 +1,22 @@
+package schema
+
+import (
+	"fmt"
+	"testing"
+)
+
+func AssertEqual(t *testing.T, expected interface{}, actual interface{}) {
+	if expected != actual {
+		t.Errorf("Expected '%s' got '%s'", expected, actual)
+	}
+}
+
+var printed_enum = `enum Hello {
+  Opt1,
+  Opt2,
+}`
+
+func TestEnumPrinting(t *testing.T) {
+	enum := Enum{"Hello", []Name{"Opt1", "Opt2"}}
+	AssertEqual(t, printed_enum, fmt.Sprint(enum))
+}
