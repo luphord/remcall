d07ef75d2c8701842e7a542f0d58c78819f77b89
luphord <luphord@protonmail.com>
2019-02-20 17:16:21 +0100

    setuptools based setup.py + HISTORY

--- setup.py
+++ setup.py
@@ -1,10 +1,49 @@
-from distutils.core import setup
-setup(name='remcall',
-      version='0.1.0',
-      description='remcall - ipc using remote method calls and object proxying between different programming languages',
-      author='luphord',
-      author_email='luphord@protonmail.com',
-      license="MIT license",
-      url='https://github.com/luphord/remcall',
-      packages=['remcall', 'remcall.codec', 'remcall.schema', 'remcall.communication'],
-     )
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = []
+
+setup_requirements = []
+
+test_requirements = []
+
+setup(
+    author='luphord',
+    author_email='luphord@protonmail.com',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    description='ipc using remote method calls and object proxying between \
+                 different programming languages',
+    install_requires=requirements,
+    license='MIT license',
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='remcall',
+    name='remcall',
+    packages=find_packages(where='.'),
+    setup_requires=setup_requirements,
+    test_suite='test',
+    tests_require=test_requirements,
+    url='https://github.com/luphord/remcall',
+    version='0.1.0',
+    zip_safe=False,
+)
