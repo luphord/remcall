3524a881b0831f43a8ec74b03f1de52970442122
luphord <luphord@protonmail.com>
2019-02-28 07:05:52 +0100

    happy linting III

--- remcall/schema/__init__.py
+++ remcall/schema/__init__.py
@@ -4,8 +4,8 @@ from .core import Type, Interface, Enum, Record, Primitive, Method, \
                   date, datetime, time, primitive_types, Array, Schema
 from .base import assert_name
 
-__all__ = ['Type', 'Interface', 'Enum', 'Record', 'Primitive', 'Method', \
-           'string', 'int8', 'int16', 'int32', 'int64', 'uint8', 'uint16', \
-           'uint32', 'uint64', 'float32', 'float64', 'void', 'boolean', \
-           'date', 'datetime', 'time', 'assert_name', 'primitive_types', \
+__all__ = ['Type', 'Interface', 'Enum', 'Record', 'Primitive', 'Method',
+           'string', 'int8', 'int16', 'int32', 'int64', 'uint8', 'uint16',
+           'uint32', 'uint64', 'float32', 'float64', 'void', 'boolean',
+           'date', 'datetime', 'time', 'assert_name', 'primitive_types',
            'Array', 'Schema']
--- remcall/schema/core.py
+++ remcall/schema/core.py
@@ -3,6 +3,7 @@ from typing import Iterable, Tuple, Mapping, Union
 from .base import assert_name
 from .typeref import TypeRef
 
+
 class Type:
     type_order = -1
 
@@ -19,15 +20,22 @@ class Type:
 
     @property
     def sort_key(self):
-        assert self.type_order >= 0, 'Type {} has type_order {} < 0 and cannot be sorted'.format(self.name, self.type_order)
+        assert self.type_order >= 0, ('Type {} has type_order {} < 0 and ' +
+                                      'cannot be sorted') \
+                                     .format(self.name, self.type_order)
         return (self.type_order, self.name)
 
     def resolve_type_references(self, type_ref_lookup):
         pass
 
+
 TypeOrRef = Union[Type, TypeRef]
+
+
 def assert_type_or_ref(typ: TypeOrRef):
-    assert isinstance(typ, Type) or isinstance(typ, TypeRef), '{} which not an instance of Type or TypeRef'.format(typ)
+    error_msg = '{} which not an instance of Type or TypeRef'.format(typ)
+    assert isinstance(typ, Type) or isinstance(typ, TypeRef), error_msg
+
 
 class Array(Type):
     type_order = 3
@@ -38,7 +46,8 @@ class Array(Type):
 
     def __eq__(self, other: Type) -> bool:
         '''
-        Equality of array types is equaivalent to the equality of their underlying types
+        Equality of array types is equivalent
+        to the equality of their underlying types
         >>> int64 == int64
         True
         >>> Array(int64) == Array(int64)
@@ -59,15 +68,21 @@ class Array(Type):
     def __repr__(self) -> str:
         return 'Array({!s})'.format(self.typ)
 
+
 class Primitive(Type):
     def __repr__(self):
         return 'Primitive("{}")'.format(self.name)
 
-primitive_types = []
-for typename in ('void', 'boolean', 'int8', 'uint8', 'int16', 'uint16', 'int32', 'uint32', 'int64', 'uint64', 'float32', 'float64', 'string', 'date', 'time', 'datetime'):
+
+void = Primitive('void')
+primitive_types = [void]
+for typename in ('boolean', 'int8', 'uint8', 'int16', 'uint16',
+                 'int32', 'uint32', 'int64', 'uint64',
+                 'float32', 'float64', 'string', 'date', 'time', 'datetime'):
     primitive = Primitive(typename)
     primitive_types.append(primitive)
     locals()[typename] = primitive
+
 
 class Enum(Type):
     type_order = 0
@@ -75,7 +90,9 @@ class Enum(Type):
     def __init__(self, name: str, values: Iterable[str]) -> None:
         super().__init__(name)
         self.values = list(values)
-        assert len(self.values) <= 256, 'Enums may contain at most 256 values, got {}'.format(len(self.values))
+        assert len(self.values) <= 256, ('Enums may contain at most 256 ' +
+                                         'values, got {}') \
+                                        .format(len(self.values))
         for value in self.values:
             assert_name(value)
 
@@ -84,15 +101,20 @@ class Enum(Type):
         return True
 
     def pretty_print(self) -> str:
-        return 'enum {} {{\n{}\n}}'.format(self.name, '\n'.join('  {},'.format(value) for value in self.values))
+        enum_values = '\n'.join('  {},'.format(value) for value in self.values)
+        return 'enum {} {{\n{}\n}}'.format(self.name, enum_values)
 
     def __repr__(self) -> str:
-        return '{}(name="{}", values={})'.format(self.__class__.__name__, self.name, self.values)
+        return '{}(name="{}", values={})'.format(self.__class__.__name__,
+                                                 self.name, self.values)
+
 
 class Record(Type):
     type_order = 1
 
-    def __init__(self, name: str, fields: Iterable[Tuple[TypeOrRef, str]]) -> None:
+    def __init__(self,
+                 name: str,
+                 fields: Iterable[Tuple[TypeOrRef, str]]) -> None:
         super().__init__(name)
         self.fields = list(fields)
         for typ, name in self.fields:
@@ -100,7 +122,9 @@ class Record(Type):
             assert_type_or_ref(typ)
             assert typ is not void, 'Fields cannot be of type void'
 
-    def resolve_type_references(self, type_ref_lookup: Mapping[TypeRef, Type]) -> None:
+    def resolve_type_references(self,
+                                type_ref_lookup: Mapping[TypeRef, Type]) \
+            -> None:
         for field_idx, field in enumerate(list(self.fields)):
             field_type, field_name = field
             if isinstance(field_type, TypeRef):
@@ -113,13 +137,20 @@ class Record(Type):
         return True
 
     def pretty_print(self) -> str:
-        return 'record {} {{\n{}\n}}'.format(self.name, '\n'.join('  {} {};'.format(field_type, field_name) for field_type, field_name in self.fields))
+        record_fields = '\n'.join('  {} {};'.format(field_type, field_name)
+                                  for field_type, field_name in self.fields)
+        return 'record {} {{\n{}\n}}'.format(self.name, record_fields)
 
     def __repr__(self) -> str:
-        return '{}(name="{}", fields={})'.format(self.__class__.__name__, self.name, self.fields)
+        return '{}(name="{}", fields={})'.format(self.__class__.__name__,
+                                                 self.name, self.fields)
+
 
 class Method:
-    def __init__(self, name: str, arguments: Iterable[Tuple[TypeOrRef, str]], return_type: Type) -> None:
+    def __init__(self,
+                 name: str,
+                 arguments: Iterable[Tuple[TypeOrRef, str]],
+                 return_type: Type) -> None:
         assert_name(name)
         self.name = name
         self.arguments = list(arguments)
@@ -130,10 +161,15 @@ class Method:
         self.return_type = return_type
 
     def __str__(self) -> str:
-        return '{!s} {}({});'.format(self.return_type, self.name, ', '.join('{!s} {}'.format(typ, name) for typ, name in self.arguments))
+        args = ', '.join('{!s} {}'.format(typ, name)
+                         for typ, name in self.arguments)
+        return '{!s} {}({});'.format(self.return_type, self.name, args)
 
     def __repr__(self) -> str:
-        return '{}(name="{}", arguments={}, return_type="{}")'.format(self.__class__.__name__, self.name, self.arguments, self.return_type)
+        return '{}(name="{}", arguments={}, return_type="{}")' \
+               .format(self.__class__.__name__, self.name,
+                       self.arguments, self.return_type)
+
 
 class Interface(Type):
     type_order = 2
@@ -150,36 +186,58 @@ class Interface(Type):
     def methods_sorted(self):
         return sorted(self.methods, key=lambda m: m.name)
 
-    def resolve_type_references(self, type_ref_lookup: Mapping[TypeRef, Type]) -> None:
+    def resolve_type_references(self,
+                                type_ref_lookup: Mapping[TypeRef, Type]) \
+            -> None:
         for method in self.methods:
             for arg_idx, arg in enumerate(list(method.arguments)):
                 arg_type, arg_name = arg
                 if isinstance(arg_type, TypeRef):
-                    method.arguments[arg_idx] = (type_ref_lookup[arg_type], arg_name)
+                    method.arguments[arg_idx] = (type_ref_lookup[arg_type],
+                                                 arg_name)
             if isinstance(method.return_type, TypeRef):
                 method.return_type = type_ref_lookup[method.return_type]
 
     def pretty_print(self) -> str:
-        return 'interface {} {{\n{}\n}}'.format(self.name, '\n'.join('  {!s}'.format(method) for method in self.methods_sorted))
+        methods = '\n'.join('  {!s}'.format(method)
+                            for method in self.methods_sorted)
+        return 'interface {} {{\n{}\n}}'.format(self.name, methods)
 
     def __repr__(self) -> str:
-        return '{}(name="{}", methods={!r})'.format(self.__class__.__name__, self.name, self.methods)
+        return '{}(name="{}", methods={!r})'.format(self.__class__.__name__,
+                                                    self.name, self.methods)
+
 
 class Schema:
-    def __init__(self, label, types, bytes_method_ref=2, bytes_object_ref=4, sha256_digest=None):
+    def __init__(self, label, types,
+                 bytes_method_ref=2, bytes_object_ref=4,
+                 sha256_digest=None):
         self.label = label
         self.types = set(typ for typ in types if typ.is_declared)
-        assert bytes_method_ref in (1, 2, 4, 8), 'Method references have to be 1, 2, 4 or 8 bytes long, got {}'.format(bytes_method_ref)
-        assert bytes_object_ref in (1, 2, 4, 8), 'Object references have to be 1, 2, 4 or 8 bytes long, got {}'.format(bytes_method_ref)
+        assert bytes_method_ref in (1, 2, 4, 8), ('Method references have ' +
+                                                  'to be 1, 2, 4 or 8 bytes ' +
+                                                  'long, got {}') \
+                                                 .format(bytes_method_ref)
+        assert bytes_object_ref in (1, 2, 4, 8), ('Object references have ' +
+                                                  'to be 1, 2, 4 or 8 bytes ' +
+                                                  'long, got {}') \
+                                                 .format(bytes_method_ref)
         self.bytes_method_ref = bytes_method_ref
         self.bytes_object_ref = bytes_object_ref
         if sha256_digest:
-            assert len(sha256_digest) == 32, 'SHA256 digest must be 32 bytes long'
+            assert len(sha256_digest) == 32, \
+                   'SHA256 digest must be 32 bytes long'
         self.sha256_digest = sha256_digest
-        assert hasattr(self.type_schemas, 'Main'), 'Every schema requires an interface called "Main", got only {}'.format(', '.join('"{}"'.format(ifc.name) for ifc in self.interfaces_sorted))
+        assert hasattr(self.type_schemas, 'Main'), \
+            ('Every schema requires an interface called "Main", ' +
+             'got only {}') \
+            .format(', '.join('"{}"'.format(ifc.name)
+                              for ifc in self.interfaces_sorted))
         self.main_type = self.type_schemas.Main
         for ifc in self.interfaces_sorted:
-            assert len(ifc.methods) > 0, 'Every interface requires at least one method, "{}" has none'.format(ifc.name)
+            assert len(ifc.methods) > 0, \
+                   ('Every interface requires at least one method, ' +
+                    '"{}" has none').format(ifc.name)
 
     @property
     def iter_declared_types(self):
@@ -223,15 +281,19 @@ class Schema:
 
     @property
     def type_table(self):
-        assert primitive_types[0].name == 'void', 'First type with index 0 has to be void, got {}'.format(primitive_types[0])
-        table = {typ: idx for idx, typ in enumerate(primitive_types + self.declared_types)}
+        assert primitive_types[0].name == 'void', \
+               'First type with index 0 has to be void, got {}' \
+               .format(primitive_types[0])
+        table = {typ: idx for idx, typ in
+                 enumerate(primitive_types + self.declared_types)}
         for typ, idx in list(table.items()):
             table[Array(typ)] = -idx
         return table
 
     @property
     def type_schemas(self):
-        class _: pass
+        class _:
+            pass
         tps = _()
         for typ in self.declared_types:
             setattr(tps, typ.name, typ)
@@ -243,7 +305,9 @@ class Schema:
 
     @property
     def method_lookup(self):
-        return {idx: method for idx, method in enumerate(sum((iface.methods_sorted for iface in self.interfaces_sorted), []))}
+        methods = sum((iface.methods_sorted
+                       for iface in self.interfaces_sorted), [])
+        return {idx: method for idx, method in enumerate(methods)}
 
     @property
     def method_to_interface(self):
@@ -257,6 +321,10 @@ class Schema:
         return '\n\n'.join(typ.pretty_print() for typ in self.declared_types)
 
     def __repr__(self):
-        return '{}(label="{}", types={!r})'.format(self.__class__.__name__, self.label, self.types)
+        return '{}(label="{}", types={!r})'.format(self.__class__.__name__,
+                                                   self.label, self.types)
 
-__all__ = ['Type', 'Array', 'Primitive', 'Enum', 'Record', 'Method', 'Interface', 'Schema', 'primitive_types'] + [tp.name for tp in primitive_types]
+
+__all__ = ['Type', 'Array', 'Primitive', 'Enum', 'Record', 'Method',
+           'Interface', 'Schema', 'primitive_types'] + \
+          [tp.name for tp in primitive_types]
